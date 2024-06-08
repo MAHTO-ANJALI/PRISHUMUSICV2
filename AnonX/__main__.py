19cd39820858911b0e875673461df1fea62fa403
MAHTO ANJALI <159051588+MAHTO-ANJALI@users.noreply.github.com>
2024-06-08 15:33:30 +0530

    Update __main__.py

--- AnonX/__main__.py
+++ AnonX/__main__.py
@@ -52,12 +52,12 @@ async def init():
     await userbot.start()
     await Anon.start()
     try:
-        await Anon.stream_decall("https://telegra.ph/file/de3464aa7d6bfafdd2dc3.mp4")
+        await Anon.stream_decall("https://te.legra.ph/file/81d15dc81bf7d67f5b4a0.mp4")
     except:
         pass
     try:
         await Anon.stream_call(
-            "https://te.legra.ph/file/29f784eb49d230ab62e9e.mp4"
+            "https://te.legra.ph/file/774624c731c7b51b09a6b.mp4"
         )
     except NoActiveGroupCall:
         LOGGER("AnonX").error(
